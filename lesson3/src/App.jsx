33adb6913496bf81ee730f6b147e3ec7b8b0b4e4
kostiantinkovalskyi93 <koskov47@gmail.com>
2025-05-28 15:54:17 +0300

    change code

--- lesson3/src/App.jsx
+++ lesson3/src/App.jsx
@@ -1,9 +1,9 @@
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import React from 'react';
+import Header from './components/Header.jsx';
+import Sidebar from './components/Sidebar.jsx';
+import Main from './components/Main.jsx';
+import Footer from './components/Footer.jsx';
 import data from './data/data.json';
-
 import './App.css';
 
 function App() {
--- lesson3/src/components/Main.jsx
+++ lesson3/src/components/Main.jsx
@@ -1,10 +1,13 @@
+import React from 'react';
+import sections from '../data/data.json';
+
 export default function Main() {
     return (
         <main className="main">
             {sections.map(section => (
                 <section key = {section.id}>
                     <h2>{section.title}</h2>
-                    <p>{section.content}</p>
+                    <p>{section.description}</p>
                 </section>
             ))}
         </main>
