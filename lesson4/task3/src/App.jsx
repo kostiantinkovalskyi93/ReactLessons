6918baeb3962a28e6c066440358a6081d9380ae2
kostiantinkovalskyi93 <koskov47@gmail.com>
2025-05-31 17:03:01 +0300

    виніс inline-стилі в окремий файл

--- lesson4/task3/src/App.jsx
+++ lesson4/task3/src/App.jsx
@@ -9,37 +9,12 @@ function App() {
   };
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100vh',
-      backgroundColor: '#f0f0f0'
-    }}>
-      <p style={{
-        color: textColor,
-        fontSize: '24px',
-        marginBottom: '16px'
-      }}>
-        Це текст, колір якого змінюється при натисканні на кнопку!
+    <div className='container'>
+      <p style={{color: textColor}} className='text'>
+        Текст, колір якого змінюється при натисканні на кнопку.
       </p>
       <button
-        onClick={changeColor}
-        style={{
-          backgroundColor: '#007bff',
-          color: 'white',
-          padding: '12px 22px',
-          border: 'none',
-          borderRadius: '4px',
-          cursor: 'pointer',
-          fontSize: '16px',
-          fontWeight: 'bold',
-          transition: 'background-color 0.3s'
-        }}
-        onMouseOver={(e) => e.target.style.backgroundColor = '#0056b3'}
-        onMouseOut={(e) => e.target.style.backgroundColor = '#007bff'}
-      >
+        onClick={changeColor} className='button'>
         Змінити колір
       </button>
     </div>
